26f7df4a527f95e65f2f7d927ada8b8a7d81a4a8
Andrew Berlandbor <141494909+berlandbor@users.noreply.github.com>
2023-12-20 19:14:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 import IframeGenerator from './iframeGenerator.js';
 import ModalGenerator from './modalGenerator.js';
 
-const iframeGenerator = new IframeGenerator('https://berlandbor.github.io/list-txt/', 350, 600);
+const iframeGenerator = new IframeGenerator('https://berlandbor.github.io/SVG/IMG_20231124_083117%20(1).svg', 350, 600);
 const modalGenerator = new ModalGenerator(iframeGenerator);
 
 const openButton = document.getElementById('openButton'); // предполагается, что у вас есть кнопка с id="openButton"
